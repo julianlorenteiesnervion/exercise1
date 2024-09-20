42e4d723efc228f6a29dc0f41d8c73b5c2d01cdc
julian.lorente <julian.lorente@106-16.info2>
2024-09-20 13:44:21 +0200

    Ejercicio 1 terminado

---
+++ src/exercise1/Exercise1.java
@@ -0,0 +1,11 @@
+package exercise1;
+
+public class Exercise1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, how are you? ");
+		System.out.print("Fine, thanks.");
+
+	}
+
+}
